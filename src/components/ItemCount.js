173e2ec3e125ca13a8e8209fdb3ad69c737a2eb6
Gonzalo Galvan <gonzalongalvan2@gmail.com>
2022-07-11 23:09:54 -0300

    eventos

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import './ItemCount.css';
 
 function ItemCount({ stock }) {
     const [num, setNum] = useState(0);   
     const [countState, setCountState] = useState(true);
+    const [newStock, setNewStock] = useState("");
+
+    const stockHandler = () => {
+        setNewStock(num);
+        console.log(newStock);
+
+    }
     
     const suma = () => {
         if(num<stock){
             setNum(num + 1);
+            stockHandler();
         }
         else {
             alert(`El stock de este producto es de ${stock} unidades`);
@@ -17,14 +25,11 @@ function ItemCount({ stock }) {
 
     const resta = () => {
         if(num>0){
-            setNum(num - 1)
+            setNum(num - 1);
+            stockHandler();
         }
     } 
-    const [newStock, setNewStock] = useState()
-    const stockHandler = () => {
-        setNewStock(num)
-        console.log(newStock);
-    }
+    
     
 
     return (
@@ -33,8 +38,8 @@ function ItemCount({ stock }) {
             <div className="contador">
                 <h3>{num}</h3>
                 <p className="textoStock">Stock disponible: {stock}</p>
-                <button onClick={resta, stockHandler} >-</button>
-                <button onClick={suma, stockHandler}>+</button>
+                <button onClick={resta}>-</button>
+                <button onClick={suma}>+</button>
             </div>
             <button className="agregar" onClick={ () => setCountState(false) }  >Agregar</button>
         </>
