bad7438ea2103a61db4c2c4d33920be1735a7d3e
Gonzalo Galvan <gonzalongalvan2@gmail.com>
2022-08-01 21:05:06 -0300

    entrega final

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -7,31 +7,13 @@ import { getFirestore, doc, getDoc, collection } from 'firebase/firestore';
 function ItemDetailContainer() {
 
   const [productosDetailFecth, setProductosDetailFetch] = useState({});
-  const {id} = useParams()
-
-  // useEffect(() => {
-  //   const getItems = new Promise((resolve) =>{
-  //     setTimeout(() => {
-  //       fetch('/productos.json')
-  //       .then(response => response.json())
-  //       .then((result) => {
-  //         const myProducts = result.find((product) => product.id === productId);
-  //         resolve(myProducts);
-  //       });
-  //     }, 2000);
-  //   });
-
-  //   getItems.then((res) =>{
-  //     setProductosDetailFetch(res);
-  //   });
-  // }, []);
-
+  const {productId} = useParams()
 
   useEffect(() => {
     const db = getFirestore();
 
     const productsCollectionRef = collection(db, "productos")
-    const productRef = doc(productsCollectionRef);
+    const productRef = doc(productsCollectionRef, productId);
     
     getDoc(productRef).then((snapshot) => {
       if(snapshot.exists()){
@@ -39,15 +21,7 @@ function ItemDetailContainer() {
        
       }
     })
-
-    // getDoc(productRef).then((snapshot) => {
-    //   snapshot.id = productId
-    //   if(snapshot.exist()){
-    //     setProductosDetailFetch({ ...snapshot.data() });
-        
-    //     console.log(snapshot.id)
-    //   }
-    }, [id])
+    }, [productId])
 
 
   return (
